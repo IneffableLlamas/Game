17fbfc3b89e7ce4c01892be1c995c5fc34f46420
LeDoux <ben.ledoux@gmail.com>
2018-09-25 13:39:45 -0600

    Updated Coin Class

--- Coin.java
+++ Coin.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  * Coin class for Game
@@ -11,5 +13,43 @@
  */
 
 public class Coin {
+	private String coinState;
 
+	/**
+	* Constructs a Coin object with a randomized value of 'H' or 'T'.
+	*/
+	public Coin() {
+		String[] coinChoose = {"H", "T"}; // Coin options: H for heads, T for tails
+		Random random = new Random(); 
+
+		this.coinState = arr[random.nextInt(arr.length)]; // Randomly selects 0 or 1 from coinChoose and assigns value to coinState
+	}
+
+	/**
+	 * Modifies the coin state by flipping the value
+	*/
+	public void flip() {
+		if(coinState == "H") {
+			coinState = "T";
+		}
+		else {
+			coinState = "H";
+		}
+	}
+
+	/**
+	 * Provides the current state of the coin
+	 * 
+	 * @return Current Coin State
+	 */	
+	public String get_coin() {
+		return coinState;
+	}	
+
+	/**
+	 * Sets the state of the coin to a new state provided by the engine
+	 * @param newState	new state of the coin as input by the engine
+	public void set_coin(String newState) {
+		coinState = newState;
+	}
 }
