81e0010355823a7e524219adc5820547e3820240
LeDoux <ben.ledoux@gmail.com>
2018-09-25 13:53:04 -0600

    Start of Wheel Class

--- Wheel.java
+++ Wheel.java
@@ -9,3 +9,22 @@
  * @author Anh Nguyen
  * @version 1.0
  */
+
+public class Wheel {
+	private Coin[] coinWheel;
+
+	public Wheel(int sections) {
+		this.coinWheel = new Coin[sections];
+		for(int i = 0; i < coinWheel.length; i++) {
+			coinWheel[i] = new Coin();
+		}
+	}
+
+	public Coin[] get_wheel() {
+		
+	}
+
+	public void spin() {
+
+	}
+} 
