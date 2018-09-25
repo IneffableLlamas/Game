5971209519a022c2b22941a00b44311579b6e6c9
LeDoux <ben.ledoux@gmail.com>
2018-09-25 10:47:54 -0600

    Create Wheel and Coin Java Files

--- Coin.java
+++ Coin.java
@@ -1,1 +1,15 @@
-# Coin class
+/**
+ * 
+ * Coin class for Game
+ *
+ *
+ * @author Ben LeDoux
+ * @author Jimmy Morgan
+ * @author Mark Huntington
+ * @author Anh Nguyen
+ * @version 1.0
+ */
+
+public class Coin {
+
+}
--- Wheel.java
+++ Wheel.java
@@ -1,1 +1,11 @@
-# Wheel Class
+/**
+ *
+ * Wheel class for Game
+ *
+ *
+ * @author Ben LeDoux
+ * @author Jimmy Morgan
+ * @author Mark Huntington
+ * @author Anh Nguyen
+ * @version 1.0
+ */
