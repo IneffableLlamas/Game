431841b3c3568b61262ddfe1eb7bda62dabc6711
mrmorgan <mr.jmmorgan@gmail.com>
2018-09-27 11:15:25 -0600

    added main class

---
+++ Game.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Main class for Game
+ *
+ *
+ * @author Ben LeDoux
+ * @author Jimmy Morgan
+ * @author Mark Huntington
+ * @author Anh Nguyen
+ * @version 1.0
+ */
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
