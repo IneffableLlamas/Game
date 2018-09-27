63c9b2bd91e4da9ffa78af8ec121eaff26ef5280
mrmorgan <mr.jmmorgan@gmail.com>
2018-09-27 11:03:45 -0600

    changed String to char

--- Coin.java
+++ Coin.java
@@ -13,13 +13,13 @@ import java.util.Random;
  */
 
 public class Coin {
-	private String coinState;
+	private char coinState;
 
 	/**
 	* Constructs a Coin object with a randomized value of 'H' or 'T'.
 	*/
 	public Coin() {
-		String[] coinChoose = {"H", "T"}; // Coin options: H for heads, T for tails
+		char[] coinChoose = {'H', 'T'}; // Coin options: H for heads, T for tails
 		Random random = new Random(); 
 
 		this.coinState = coinChoose[random.nextInt(coinChoose.length)]; // Randomly selects 0 or 1 from coinChoose and assigns value to coinState
@@ -29,11 +29,11 @@ public class Coin {
 	 * Modifies the coin state by flipping the value
 	*/
 	public void flip() {
-		if(coinState == "H") {
-			coinState = "T";
+		if(coinState == 'H') {
+			coinState = 'T';
 		}
 		else {
-			coinState = "H";
+			coinState = 'H';
 		}
 	}
 
@@ -42,7 +42,7 @@ public class Coin {
 	 * 
 	 * @return Current Coin State
 	 */	
-	public String get_coin() {
+	public char get_coin() {
 		return coinState;
 	}	
 
@@ -50,7 +50,7 @@ public class Coin {
 	 * Sets the state of the coin to a new state provided by the engine
 	 * @param newState	new state of the coin as input by the engine
 	*/
-	public void set_coin(String newState) {
+	public void set_coin(char newState) {
 		coinState = newState;
 	}
 }
