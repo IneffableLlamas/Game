59a984c9a0f0686f0ffdf72345a2ab78fde8fa84
- <->
2018-10-02 13:38:20 -0600

    engine class

---
+++ engine.java
@@ -0,0 +1,44 @@
+/**
+ *
+ * engine class
+ *
+ *
+ * @author Ben LeDoux
+ * @author Jimmy Morgan
+ * @author Mark Huntington
+ * @author Anh Nguyen
+ * @version 1.0
+ */
+
+public class engine {
+	
+	
+	int [] turn = new turn[] ;
+	
+	int [] revealed = new revealed [];
+	
+	public make_Wheel() {
+		
+		// Calling Wheel class
+		
+		Wheel wheel = new Wheel();
+		
+		
+	}
+	
+	public play_Game() {
+		
+	}
+	
+	public boolean check_Win() {
+		
+		return true;
+	}
+	
+	
+	public String display_Wheel() {
+		
+		return "some string" ;
+	}
+	
+}
